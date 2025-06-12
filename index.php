03e7772a292f8a981c03cf5dfce8238ee65c0d9d
Trudy <144907774+trudyannroberts@users.noreply.github.com>
2025-06-12 15:53:18 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,14 @@
 <?php
-echo "<h1>Hello from PHP!</h1>";
-?>
+$host = "dokploy.usn.no";
+$user = "888846";
+$pass = "71f2888846";
+$db   = "888846";
 
+$conn = new mysqli($host, $user, $pass, $db);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+echo "Connected to MySQL successfully!";
+?>
