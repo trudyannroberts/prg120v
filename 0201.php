5d253b8adc86d30ef51387b9cb5ef32eecd43eed
Trudy Ann Roberts <trudyannroberts@gmail.com>
2025-08-09 14:47:24 +0200

    0201

---
+++ 0201.php
@@ -0,0 +1,15 @@
+<?php /* Oppgave 1 */
+/*
+/* Programmet mottar fra et HTML-skjema et svar på spørsmålet "Hva er 3 ganger 3 ?"
+/* Programmet sjekker om svaret er riktig og skriver ut en melding avhengig av om svaret er riktig eller feil
+*/
+$tall=$_POST ["svar"];
+if ($svar == 9) /* avgitt svar er riktig */
+{
+print("Riktig. 3 ganger 3 er 9 <br/>");
+}
+else /* avgitt svar er feil */
+{
+print("Feil. 3 ganger 3 er ikke $svar. 3 ganger 3 er 9 <br/>");
+}
+?>
