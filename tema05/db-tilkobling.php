4f0137c7cd29ba175cbcbcc5e540d7a7365c7f2e
Trudy Ann Roberts <trudyannroberts@gmail.com>
2025-09-15 21:34:51 +0200

    jbdf

--- tema05/db-tilkobling.php
+++ tema05/db-tilkobling.php
@@ -3,9 +3,10 @@
 /* Programmet foretar tilkobling til database-server og valg av database
 */
 $host = getenv("b-studentsql-1.usn.no");
+$port = 3306;  
 $username = getenv('888846');
 $password = getenv('71f2888846');
 $database = getenv('888846');
-$db=mysqli_connect($host,$username,$password,$database) or die ("ikke kontakt med database-server");
+$db=mysqli_connect($host,$port, $username,$password,$database) or die ("ikke kontakt med database-server");
 /* tilkobling til database-serveren utført */
 ?>
