734e7325d375d33b955a42bd86af5a84d143232f
Trudy Ann Roberts <trudyannroberts@gmail.com>
2025-09-15 15:03:10 +0200

    b

--- tema05/db-tilkobling.php
+++ tema05/db-tilkobling.php
@@ -2,7 +2,7 @@
 /*
 /* Programmet foretar tilkobling til database-server og valg av database
 */
-$host = getenv('localhost');
+$host = getenv("b-studentsql-1.usn.no");
 $username = getenv('888846');
 $password = getenv('71f2888846');
 $database = getenv('888846');
