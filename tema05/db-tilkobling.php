cc13111b1f6b575fbaefd4ae05bbf315d8aceacf
Trudy Ann Roberts <trudyannroberts@gmail.com>
2025-09-16 10:52:02 +0200

    jdfh

--- tema05/db-tilkobling.php
+++ tema05/db-tilkobling.php
@@ -2,10 +2,10 @@
 /*
 /* Programmet foretar tilkobling til database-server og valg av database
 */
-$host = getenv("b-studentsql-1.usn.no");
-$username = getenv('888846');
-$password = getenv('71f2888846');
-$database = getenv('888846');
+$host = getenv('DB_HOST');
+$username = getenv('DB_USER');
+$password = getenv('DB_PASSWORD');
+$database = getenv('DB_DATABASE');
 $db=mysqli_connect($host, $username,$password,$database, 3306) or die ("ikke kontakt med database-server");
 /* tilkobling til database-serveren utført */
 ?>
