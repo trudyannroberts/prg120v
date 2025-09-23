ba7b3704c0a247e14059e335033d78ed85f003f9
Trudy Ann Roberts <trudyannroberts@gmail.com>
2025-09-23 11:20:25 +0200

    jh

--- tema05-06/registrer-studium.php
+++ tema05-06/registrer-studium.php
@@ -1,48 +1,42 @@
-<?php /* registrer-emne */
+<?php /* registrer-studium */
 /*
-/* Programmet lager et html-skjema for å registrere et emne
-/* Programmet registrerer data i databasen
+/* Programmet lager et html-skjema for å registrere et studium
+/* Programmet registrerer data (studiumkode og studiumnavn) i databasen
 */
 ?>
-<h3>Registrer emne </h3>
-<form method="post" action="" id="registrerEmneSkjema" name="registrerEmneSkjema">
-Emnekode <input type="text" id="emnekode" name="emnekode" required /> <br/>
-Emnenavn <input type="text" id="emnenavn" name="emnenavn" required /> <br/>
-Studiumkode <select name="studiumkode" id="studiumkode">
-<?php print("<option value=''>velg studium </option>");
-include("dynamiske-funksjoner.php"); listeboksStudiumkode(); ?>
-</select> <br/>
-<input type="submit" value="Registrer emne" id="registrerEmneKnapp" name="registrerEmneKnapp" />
+<h3>Registrer studium </h3>
+<form method="post" action="" id="registrerStudiumSkjema" name="registrerStudiumSkjema">
+Studiumkode <input type="text" id="studiumkode" name="studiumkode" required /> <br/>
+Studiumnavn <input type="text" id="studiumnavn" name="studiumnavn" required /> <br/>
+<input type="submit" value="Registrer studium" id="registrerStudiumKnapp" name="registrerStudiumKnapp" />
 <input type="reset" value="Nullstill" id="nullstill" name="nullstill" /> <br />
 </form>
-
 <?php
-if (isset($_POST ["registrerEmneKnapp"]))
+if (isset($_POST ["registrerStudiumKnapp"]))
 {
-$emnekode=$_POST ["emnekode"];
-$emnenavn=$_POST ["emnenavn"];
 $studiumkode=$_POST ["studiumkode"];
-if (!$emnekode || !$emnenavn || !$studiumkode)
+$studiumnavn=$_POST ["studiumnavn"];
+if (!$studiumkode || !$studiumnavn)
 {
 print ("Alle felt m&aring; fylles ut");
 }
 else
 {
-include("db-tilkobling.php"); /* tilkobling til database-serveren utført og valg av database foretatt */
-$sqlSetning="SELECT * FROM emne WHERE emnekode='$emnekode';";
+include("../db-tilkobling.php"); /* tilkobling til database-serveren utført og valg av database foretatt */
+$sqlSetning="SELECT * FROM studium WHERE studiumkode='$studiumkode';";
 $sqlResultat=mysqli_query($db,$sqlSetning) or die ("ikke mulig &aring; hente data fra databasen");
 $antallRader=mysqli_num_rows($sqlResultat);
-if ($antallRader!=0) /* faget er registrert fra før */
+if ($antallRader!=0) /* studiet er registrert fra før */
 {
-print ("Emnet er registrert fra f&oslashr");
+print ("Studiet er registrert fra f&oslashr");
 }
 else
 {
-$sqlSetning="INSERT INTO emne (emnekode,emnenavn,studiumkode)
-VALUES('$emnekode','$emnenavn','$studiumkode');";
+$sqlSetning="INSERT INTO studium (studiumkode,studiumnavn)
+VALUES('$studiumkode','$studiumnavn');";
 mysqli_query($db,$sqlSetning) or die ("ikke mulig &aring; registrere data i databasen");
-print ("F&oslash;lgende emne er n&aring; registrert: $emnekode $emnekode $studiumkode");
+/* SQL-setning sendt til database-serveren */
+print ("F&oslash;lgende studium er n&aring; registrert: $studiumkode $studiumnavn");
 }
 }
 }
-?>
