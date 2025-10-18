9503b39fccae3f0aabc730e9de27f0ed14876bfe
Trudy Ann Roberts <trudyannroberts@gmail.com>
2025-10-18 15:22:02 +0200

    kjhg

--- tema02/0208.php
+++ tema02/0208.php
@@ -15,16 +15,12 @@
 </html>
 
 <?php
-if (isset($_POST ["fortsett"])) 
-{
-$svar=$_POST ["svar"];
-if ($svar == 9)
-{
-print("Riktig. 3 ganger 3 er 9 <br/>");
-}
-else
-{
-print("Feil. 3 ganger 3 er ikke $svar. 3 ganger 3 er 9 <br/>");
-}
-}
+  if (isset($_POST ["fortsett"])) {
+    $svar=$_POST ["svar"];
+    if ($svar == 9){
+      print("Riktig. 3 ganger 3 er 9 <br/>");
+    } else {
+      print("Feil. 3 ganger 3 er ikke $svar. 3 ganger 3 er 9 <br/>");
+    }
+  }
 ?>
